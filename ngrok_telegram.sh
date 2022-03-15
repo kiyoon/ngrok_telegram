a3c0b6124c062723f50c9dedc4b08b15242d7d18
Kiyoon Kim <yoonkr33@gmail.com>
2022-03-15 14:03:39 +0000

    make it wait until it shows results

--- ngrok_telegram.sh
+++ ngrok_telegram.sh
@@ -9,4 +9,14 @@ while ! nc -z localhost 4040; do
   sleep 0.2 # wait Ngrok to be available
 done
 
-python3 telegram_post.py --title 'Ngrok URL' --body "$(python3 "$SCRIPT_DIR/read_ngrok_url.py")"
+ngrok_urls="$(python3 "$SCRIPT_DIR/read_ngrok_url.py")"
+
+# Do it until it shows some info.
+while [[ $(echo "$ngrok_urls" | wc -l) -eq 1 ]]
+do
+	sleep 1
+	ngrok_urls="$(python3 "$SCRIPT_DIR/read_ngrok_url.py")"
+done
+
+
+python3 telegram_post.py --title 'Ngrok URL' --body "$ngrok_urls"
